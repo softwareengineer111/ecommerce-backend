42f1d3ae60e3898f67ae08e0e58ff8481f3829b5
aagii <batgerel@stora.mn>
2025-09-08 11:59:08 +0800

    shop manager

--- models/User.js
+++ models/User.js
@@ -1,11 +1,17 @@
-
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['user','admin'], default: 'user' },
-}, { timestamps: true });
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ['user', 'admin', 'shop-manager', 'superadmin'],
+      default: 'user',
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('User', UserSchema);
